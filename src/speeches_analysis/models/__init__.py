7ee4818a22bdf1b8ae25791dba6df1cccb9c83fe
matteusgui <matteusgui@hotmail.com>
2024-09-24 20:59:40 -0300

    Models for the data extraction

---
+++ src/speeches_analysis/models/__init__.py
@@ -0,0 +1,4 @@
+import speeches_analysis.models.deputado as deputy
+import speeches_analysis.models.partido as party
+import speeches_analysis.models.discurso as speech
+import speeches_analysis.models.faseEvento as eventPhase
---
+++ src/speeches_analysis/models/faseEvento.py
@@ -0,0 +1,8 @@
+from pydantic import Field
+import speeches_analysis.models.baseModel as baseModel
+
+
+class FaseEvento(baseModel.BaseClass):
+    data_hora_fim: str = Field(alias="dataHoraFim")
+    data_hora_inicio: str = Field(alias="dataHoraInicio")
+    titulo: str = Field(alias="titulo")
