a792ca0dde8bc111a55b392ead78c94ab27d7cd0
matteusgui <matteusgui@hotmail.com>
2024-10-15 22:01:21 -0300

    Set up the init files

---
+++ src/speeches_scrap/models/__init__.py
@@ -0,0 +1,4 @@
+import speeches_scrap.models.deputado as deputy
+import speeches_scrap.models.partido as party
+import speeches_scrap.models.discurso as speech
+import speeches_scrap.models.faseEvento as eventPhase
