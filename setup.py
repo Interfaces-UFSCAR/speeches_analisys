16f4bc5e2deadd17c0aa1753107208ec485d8876
matteusgui <matteusgui@hotmail.com>
2024-10-17 22:24:36 -0300

    Update the intialization of the lib

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="speeches_analysis",
-    packages=setuptools.find_packages(include=['src/speeches_analysis']),
+    packages=setuptools.find_packages(include=['src/speeches_scrap',
+                                               'src/extract_topics']),
     version='0.1.0',
     description="A analysis tool for the brazilian chmaber of deputies speeches",
     author="Matteus Guilherme de Souza",
---
+++ src/extract_topics/__init__.py
@@ -0,0 +1,2 @@
+from extract_topics.models.bow_lda import BowLda
+from extract_topics.models.tfidf_pbg import TfidfPbg
