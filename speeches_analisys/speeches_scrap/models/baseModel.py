16380a866f9acf2db05efd2c199ca28dc012a397
matteusgui <matteusgui@hotmail.com>
2024-11-28 20:59:46 -0300

    This commit makes the model hashable in order to use them as key for dictionaries

--- speeches_analisys/speeches_scrap/models/baseModel.py
+++ speeches_analisys/speeches_scrap/models/baseModel.py
@@ -2,4 +2,5 @@ from pydantic import BaseModel, ConfigDict
 
 
 class BaseClass(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True,
+                              frozen=True)
